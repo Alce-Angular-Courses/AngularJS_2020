10f84d17dea5d99031d4045ada3254914931fee2
alce65 <alejandro.cerezo@evosoft.es>
2020-03-02 19:28:22 +0100

    Eleemntos de ES6

---
+++ 02_ES6/funciones.js
@@ -0,0 +1,38 @@
+
+
+// declaracion
+function suma (a,b) {
+    return a + b
+}
+
+// asignacion
+
+const resta = function (a, b) {
+    return a - b 
+}
+
+console.log(suma(2,6))
+console.log(resta(6,7))
+
+// ES6 Arrow Functions o Lambdas
+
+
+// const prod = (a,b) => { return a*b}
+const prod = (a,b) => a*b
+const cuadrado = a => a*a
+
+console.log(prod(6,5))
+console.log(cuadrado(9))
+
+console.log(prod)
+
+
+setTimeout( () => console.log('Pintando'), 2000)
+
+const datos = [6, 7, 5, 3]
+
+const cuadrados = datos.map(item => item*item)
+
+console.log(cuadrados)
+
+
